7fc5cab8432b9694e7c601fa80fc0c268d26f8ec
igor <150636747+ehoiguinho@users.noreply.github.com>
2026-09-15 22:43:07 -0300

    refactor: separa controllers e services

--- crudUsuarios/controllers/usuarioController.cs
+++ crudUsuarios/controllers/usuarioController.cs
@@ -6,17 +6,15 @@ using Microsoft.EntityFrameworkCore;
 
 public class UsuarioController : ControllerBase
 {   
-    private readonly AppDbContext _context;
-
-    public UsuarioController(AppDbContext context)
+    private readonly UsuarioService _usuarioService;
+    public UsuarioController(UsuarioService usuarioService)
     {
-        _context = context;
-        
+        _usuarioService = usuarioService;        
     }
     [HttpGet]
-    public async Task <IActionResult> GetUsuario()
+    public async Task <IActionResult> GetUsuarios()
     {
-        var usuarios = await _context.Usuarios.ToListAsync();
+        var usuarios = await _usuarioService.GetUsuarios();
 
         return Ok(usuarios);
     }
@@ -24,7 +22,7 @@ public class UsuarioController : ControllerBase
     [HttpGet("{id}")]
     public async Task <IActionResult> GetUsuario(int id)
     {
-            var usuario = await _context.Usuarios.FirstOrDefaultAsync(u => u.Id == id);
+            var usuario = await _usuarioService.GetUsuarioId(id);
         if(usuario == null)
         {
             return NotFound();
@@ -34,9 +32,7 @@ public class UsuarioController : ControllerBase
     [HttpPost]
     public async Task <IActionResult> PostUsuario(Usuario usuario)
     {
-        await _context.Usuarios.AddAsync(usuario);
-
-        await _context.SaveChangesAsync();
+        await _usuarioService.PostUsuario(usuario);
 
         return CreatedAtAction("GetUsuario", new { id = usuario.Id}, usuario);
         
@@ -44,36 +40,26 @@ public class UsuarioController : ControllerBase
     [HttpPut("{id}")]
     public async Task <IActionResult> PutUsuario(int id, Usuario usuario)
     {
-        var usuarioExistente = await _context.Usuarios.FirstOrDefaultAsync( u => u.Id == id);
-        if(usuarioExistente  == null)
+        var usuarioAlterado = await _usuarioService.PutUsuario(id, usuario);
+       
+       if(usuarioAlterado == null)
         {
-            return NotFound();
+        return NotFound();
+
         }
-        // usuario que veio da url é comparado com o usuario encontrado no banco
-        usuarioExistente.Nome = usuario.Nome;
-        usuarioExistente.Email = usuario.Email;
-        usuarioExistente.Senha = usuario.Senha;
-        usuarioExistente.Telefone = usuario.Telefone;
-
-        await _context.SaveChangesAsync();
-
-        return Ok(usuarioExistente);
+        return Ok(usuario);
 
     }
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteUsuario(int id)
     {
-        var usuarioExistente = await _context.Usuarios.FirstOrDefaultAsync( u => u.Id == id);
+        var usuarioExistente = await _usuarioService.DeleteUsuario(id);
         if(usuarioExistente == null)
         {
             return NotFound();
         }
 
-        _context.Usuarios.Remove(usuarioExistente);
-
-        await _context.SaveChangesAsync();
-
-        return Ok();
+        return Ok(usuarioExistente);
     }
 
     
--- crudUsuarios/services/usuarioService.cs
+++ crudUsuarios/services/usuarioService.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 
 public class UsuarioService{
 
@@ -14,6 +15,55 @@ public class UsuarioService{
         var usuarios = await _context.Usuarios.ToListAsync();
 
         return usuarios;
+    }
+
+    public async Task <Usuario?> GetUsuarioId(int id)
+    {
+        var usuario = await _context.Usuarios.FirstOrDefaultAsync(u => u.Id == id);
+       
+        return usuario;
+
+    }
+    public async Task <Usuario>PostUsuario(Usuario usuario)
+    {
+        await _context.Usuarios.AddAsync(usuario);
+
+        await _context.SaveChangesAsync();
+
+        return usuario;
+    }
+    public async Task <Usuario?>PutUsuario(int id, Usuario usuario)
+    {
+        var usuarioExistente = await _context.Usuarios.FirstOrDefaultAsync(u => u.Id == id);
+        
+        if (usuarioExistente == null)
+        {
+            return null;
+        }
+
+        usuarioExistente.Nome = usuario.Nome;
+        usuarioExistente.Email = usuario.Email;
+        usuarioExistente.Senha = usuario.Senha;
+        usuarioExistente.Telefone = usuario.Telefone;
+
+        await _context.SaveChangesAsync();
+
+        return usuarioExistente;
+    }
+    public async Task <Usuario?>DeleteUsuario(int id)
+    {
+        var usuarioExistente = await _context.Usuarios.FirstOrDefaultAsync(u => u.Id == id);
+        if(usuarioExistente == null)
+        {
+            return null;
+        }
+
+        _context.Usuarios.Remove(usuarioExistente);
+
+        await _context.SaveChangesAsync();
+
+        return usuarioExistente;
+        
     }
 
 }
